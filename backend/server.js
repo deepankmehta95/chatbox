769b754f0c30bb3f41520e229678634ec889f6e7
Deepank Mehta <deepankmehta@Deepanks-MacBook-Pro.local>
2022-08-23 19:10:28 -0400

    finished socket.io backend implementation

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,55 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold))
+const server = app.listen(
+  PORT,
+  console.log(`Server started on PORT ${PORT}`.yellow.bold)
+)
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: 'http://localhost:8080',
+  },
+})
+
+// Socket IO
+io.on('connection', (socket) => {
+  console.log(`Connect to socket.io`)
+
+  socket.on('setup', (userData) => {
+    socket.join(userData._id)
+    console.log(userData._id)
+    socket.emit('connected')
+  })
+
+  socket.on('join chat', (room) => {
+    socket.join(room)
+    console.log(`User joined room: ${room}`)
+  })
+
+  socket.on('typing', (room) => {
+    socket.in(room).emit('typing')
+  })
+
+  socket.on('stop typing', (room) => {
+    socket.in(room).emit('stop typing')
+  })
+
+  socket.on('new message', (newMessageReceived) => {
+    let chat = newMessageReceived.chat
+
+    if (!chat.users) return console.log('chat.users not defined')
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return
+
+      socket.in(user._id).emit('message received', newMessageReceived)
+    })
+  })
+
+  socket.off('setup', () => {
+    console.log('User disconnected')
+    socket.leave(userData._id)
+  })
+})
